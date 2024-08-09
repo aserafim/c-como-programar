2051baf80ec0c17ea80e9118cce4cc8aeb895343
aserafim <correia.alefe@gmail.com>
2024-08-09 11:15:07 -0300

    Primeiros passos em C

---
+++ ch-02/hello-world.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(void) {
+    printf("Bem-vindo a C!\n");
+
+    return 0;
+}
---
+++ soma_inteiros.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+
+    int inteiro1, inteiro2, soma;
+
+    printf("Insira o primeiro inteiro: \n");
+    scanf("%d", &inteiro1);
+
+    return 0;
+}
