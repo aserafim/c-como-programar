81380bf6f8671e1332246b7ba892a55626661e29
aserafim <correia.alefe@gmail.com>
2024-08-09 11:33:39 -0300

    Somando inteiros

---
+++ ch-02/soma_inteiros.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+
+    int inteiro1, inteiro2, soma;
+
+    printf("Insira o primeiro inteiro: ");
+    scanf("%d", &inteiro1);
+
+    printf("Insira o segundo inteiro: ");
+    scanf("%d", &inteiro2);
+
+    soma = inteiro1 + inteiro2;
+
+    printf("O resultado da soma é: %d\n", soma);
+
+    return 0;
+}
